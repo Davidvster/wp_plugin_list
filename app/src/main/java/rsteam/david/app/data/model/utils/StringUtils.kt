b003c1b4efa8282a6687376930859ea4528b802c
Davidvster <valic.david@gmail.com>
2019-02-10 23:54:41 +0100

    Update StringUtils.kt

--- app/src/main/java/rsteam/david/app/data/model/utils/StringUtils.kt
+++ app/src/main/java/rsteam/david/app/data/model/utils/StringUtils.kt
@@ -5,7 +5,7 @@ object StringUtils {
         val unit = 1024
         if (bytes < unit) return "$bytes B"
         val exp = (Math.log(bytes.toDouble()) / Math.log(unit.toDouble())).toInt()
-        val pre = ("KMGTPE")[exp - 1] + "i"
+        val pre = ("KMGTPE")[exp - 1]
         return String.format("%.1f %sB", bytes / Math.pow(unit.toDouble(), exp.toDouble()), pre)
     }
 }
